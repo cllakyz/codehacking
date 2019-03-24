9be8edb94fcf14f6fa3b8b2a1f6fc3d567734ceb
Celal <cllakyz@hotmail.com>
2019-03-24 18:46:20 +0300

    admin master page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,4 +19,8 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
+Route::get('admin', function (){
+    return view('admin.index');
+});
+
 Route::resource('admin/users', 'AdminUsersController');
