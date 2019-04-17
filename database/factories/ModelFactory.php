e13e4263d45c73c53bed40c65226cb11cccb2d53
Celal <cllakyz@hotmail.com>
2019-04-18 00:26:53 +0300

    db seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,5 +17,58 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+        'role_id' => $faker->numberBetween(1,3),
+        'photo_id' => 1,
+        'status' => 1,
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'category_id' => $faker->numberBetween(1,10),
+        'photo_id' => 83,
+        'title' => $faker->sentence(7,11),
+        'body' => $faker->paragraphs(rand(10,15), true),
+        'slug' => $faker->slug(),
+    ];
+});
+
+$factory->define(App\Role::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->randomElement(['administrator', 'author', 'subscriber']),
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->randomElement(['PHP', 'Programming', 'Javascript', 'React', 'Vue', 'Laravel', 'Java']),
+    ];
+});
+
+$factory->define(App\Photo::class, function (Faker\Generator $faker) {
+    return [
+        'file' => 'placeholder.jpg'
+    ];
+});
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'post_id' => $faker->numberBetween(1,10),
+        'status' => 1,
+        'author' => $faker->name,
+        'photo' => 'placeholder.jpg',
+        'email' => $faker->safeEmail,
+        'body' => $faker->paragraphs(1, true),
+    ];
+});
+
+$factory->define(App\CommentReply::class, function (Faker\Generator $faker) {
+    return [
+        'comment_id' => $faker->numberBetween(1,10),
+        'status' => 1,
+        'author' => $faker->name,
+        'photo' => 'placeholder.jpg',
+        'email' => $faker->safeEmail,
+        'body' => $faker->paragraphs(1, true),
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'      => str_random(10),
+            'email'     => str_random(10).'@codehacking.com',
+            'password'  => bcrypt('secret'),
+            'role_id'   => 2,
+            'status'    => 1,
+        ]);
+    }
+}
