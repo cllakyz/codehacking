538f316d706dabbdcb168175df19daf56d67f394
Celal <cllakyz@hotmail.com>
2019-04-18 23:38:35 +0300

    admin layout

---
+++ resources/views/includes/admin_navbar.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    @include('includes.admin_topbar')
+    @include('includes.admin_sidebar')
+    <!-- /.navbar-static-side -->
+</nav>
